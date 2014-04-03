5abf8d816ba677849c4627f8cdfed07898aedf60
Mark Brown <markjbrown0@gmail.com>
2014-04-03 10:43:36 -0700

    added AppInsights
    blah

--- index.php
+++ index.php
@@ -17,6 +17,13 @@ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
 </style>
 
 </head>
+<!-- Copy this script into your html file to enable Application Insights -->
+<script type="text/javascript">
+    window.appInsights={queue:[],applicationInsightsId:null,accountId:null,appUserId:null,configUrl:null,start:function(n){function u(n,t){n[t]=function(){var i=arguments;n.queue.push(function(){n[t].apply(n,i)})}}function f(n){var t=document.createElement("script");return t.type="text/javascript",t.src=n,t.async=!0,t}function r(){i.appendChild(f("//az416426.vo.msecnd.net/scripts/ai.0.js"))}var i,t;this.applicationInsightsId=n;u(this,"logEvent");u(this,"logPageView");i=document.getElementsByTagName("script")[0].parentNode;this.configUrl===null?r():(t=f(this.configUrl),t.onload=r,t.onerror=r,i.appendChild(t));this.start=function(){}}};
+
+    appInsights.start("37c3db8e-1251-4db0-8ee0-67dd959b4ac7");
+    appInsights.logPageView();
+</script>
 <body>
 <h1>Say Hello!</h1>
 <p>Fill in your name, then click <strong>Submit</strong> to Say Hello.</p>
